e14e02333f09992cef1c2005946cb945d7eb6ac8
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-16 19:51:16 +0600

    Added dynamic lesson and vocabulary loading feature

--- script/index.js
+++ script/index.js
@@ -16,6 +16,17 @@ const displayLevelWord = (words) =>{
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
+  if (words.length == 0){
+    wordContainer.innerHTML = `
+    <div class="text-center bg-sky-100 col-span-full rounded py-10 space-y-6 font-bangla">
+    <img class="mx-auto" src="./assets/alert-error.png"/>
+        <p class="text-xl font-medium text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl"> নেক্সট Lesson এ যান</h2>
+      </div>
+    `;
+    return;
+  }
+
 //   {
 //     "id": 80,
 //     "level": 1,
@@ -29,9 +40,9 @@ const displayLevelWord = (words) =>{
     card.innerHTML = `
       <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-5">
         <h2 
-        class="font-bold text-2xl">${word.word}</h2>
+        class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি।"}</h2>
         <p class="font-semibold">meaning / pronunciation</p>
-        <div class="text-2xl font-medium font-bangla">${word.meaning} /${word.pronunciation}</div>
+        <div class="text-2xl font-medium font-bangla">${word.meaning ? word.meaning : " অর্থ পাওয়া যায়নি।"} /${word.pronunciation ? word.pronunciation : " প্রোনাউনসিয়েশন পাওয়া যায়নি "}</div>
         <div class="flex justify-between items-center">
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
