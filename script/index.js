e0422a16d261db7191298eade45e16a958f810d7
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-16 16:17:34 +0600

    Added dynamic word cards with pronunciation feature by level

--- script/index.js
+++ script/index.js
@@ -14,13 +14,30 @@ const loadLevLevelWord = (id) =>{
 const displayLevelWord = (words) =>{
   // console.log(words);
   const wordContainer = document.getElementById("word-container");
-  // wordContainer.innerHTML = "";
+  wordContainer.innerHTML = "";
 
+//   {
+//     "id": 80,
+//     "level": 1,
+//     "word": "Run",
+//     "meaning": "দৌড়ানো",
+//     "pronunciation": "রান"
+// }
   words.forEach((word) =>{
     console.log(word);
     const card = document.createElement("div");
     card.innerHTML = `
-    <p>Cat</p>
+      <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-5">
+        <h2 
+        class="font-bold text-2xl">${word.word}</h2>
+        <p class="font-semibold">meaning / pronunciation</p>
+        <div class="text-2xl font-medium font-bangla">${word.meaning} /${word.pronunciation}</div>
+        <div class="flex justify-between items-center">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+         
+        </div>
+      </div>
     `;
     wordContainer.append(card);
   })
